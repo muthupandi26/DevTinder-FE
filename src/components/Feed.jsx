edae6cf563287f45dcd23e4910ec58b9fad18c65
Muthupandi <p.muthupandi@tftus.com>
2025-05-25 10:22:00 +0530

    feat: added user card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [feed, setFeed] = useState([]);
+
+  const getFeed = async () => {
+    try {
+      const response = await axios.get(BASE_URL + "feed", {
+        withCredentials: true,
+      });
+      setFeed(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(feed, "FeedData");
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed.length > 0 && (
+      <div className="flex justify-center my-2">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,19 @@
+const UserCard = ({ user }) => {
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure className="px-10 pt-10">
+        <img src={user.photoUrl} alt="photo" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{`${user.firstName} ${user.lastName}`}</h2>
+        <p>About of the user!</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
